8d14e89803e61b9009a43a5f82a7b8e9fbe42058
Avikal Darolia <avikaldarolia7@gmail.com>
2021-04-23 15:03:48 +0530

    “new”

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const ToDoForm = () => {
+  const [inputItem, setInputItem] = useState("");
+  const [items, setItems] = useState([]);
+  //   const [items, updateItems] = useState([]);
 
-    
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const itemEvent = (event) => {
+    setInputItem(event.target.value);
+  };
 
-export default ToDoForm
+  const handleChange = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputItem];
+    });
+    setInputItem("");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="What ToDo??"
+        onChange={itemEvent}
+        value={inputItem}
+      />
+      <button onClick={handleChange}> + </button>
+
+      <ul>
+        {items.map((item) => {
+          <ToDoList />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDoForm;
---
+++ src/ToDoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ToDoList = (props) => {
+  return (
+    <div>
+      return <li>{props.val}</li>;
+    </div>
+  );
+};
+
+export default ToDoList;
